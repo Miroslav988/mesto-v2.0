3a80b6e9bdbda89e136e81c02468c09006adc7b4
Miroslav <mir.vbok@mail.ru>
2024-07-29 00:50:24 +0700

    чуть фич и готово

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const editUserInfoButton = document.querySelector(".edit");
 
 const submitUserBtn = document.querySelector(".submitUserBtn");
 const submitBtn = document.querySelector(".submitBtn");
+
 //===============================================================================================
 
 // DOM Elements
@@ -28,6 +29,8 @@ const cardPhotoInput = cardDataForm.elements.cardPhoto;
 
 const closeModal = () => {
   event.target.closest(".modalCover").classList.remove("modalOpened");
+  cardDataForm.reset();
+  userDataForm.reset();
 };
 forms.forEach((form) => {
   form.addEventListener("click", (event) => {
@@ -40,8 +43,18 @@ forms.forEach((form) => {
 const openModal = () => {
   const formId = event.target.dataset.form;
   const form = document.getElementById(formId);
-
   form.classList.add("modalOpened");
+
+  const overlay = document.querySelector(".modalOpened");
+  overlay.addEventListener("click", closeModal);
+
+  nameEditInput.value = userNameElement.textContent;
+  bioEditInput.value = userBioElement.textContent;
+};
+
+const openModalEnlarge = () => {
+  const enlargeContainer = document.getElementById("enlargeContainer");
+  enlargeContainer.classList.add("modalOpened");
 
   const overlay = document.querySelector(".modalOpened");
   overlay.addEventListener("click", closeModal);
@@ -49,6 +62,7 @@ const openModal = () => {
 
 const changeUserInfo = () => {
   event.preventDefault();
+
   userNameElement.textContent = nameEditInput.value;
   userBioElement.textContent = bioEditInput.value;
   userPhotoElement.src = photoEditInput.value;
@@ -62,6 +76,7 @@ const createNewCard = (name, photo) => {
   newCard.querySelector(".cardName").textContent = name;
   newCard.querySelector(".cardPhoto").alt = name;
   newCard.querySelector(".cardPhoto").src = photo;
+
   return newCard;
 };
 
@@ -70,8 +85,43 @@ const addUserCard = () => {
   const title = cardTitleInput.value;
   const photo = cardPhotoInput.value;
   const userCard = createNewCard(title, photo);
+  const deleteBtn = userCard.querySelector(".delete");
+  const like = userCard.querySelector(".like");
+  const enlargeBtn = userCard.querySelector(".enlarge");
   main.appendChild(userCard);
   closeModal();
+
+  enlargeBtn.addEventListener("click", enlargeUserCard);
+  like.addEventListener("click", switchLikeStatus);
+  deleteBtn.addEventListener("click", deleteUserCard);
+};
+
+const switchLikeStatus = () => {
+  event.preventDefault();
+  const like = event.target;
+  like.classList.contains("active")
+    ? like.classList.remove("active")
+    : like.classList.add("active");
+};
+
+const deleteUserCard = () => {
+  event.target.closest(".card").remove();
+};
+
+const enlargeUserCard = () => {
+  const enlargeCard = event.target.closest(".card");
+  const enlargeCardImg = enlargeCard.querySelector(".cardPhoto");
+  let enlargedPhoto = enlargeCardImg.src;
+  let enlargedDescription = enlargeCardImg.alt;
+
+  const enlargePhotoModal = document.querySelector(".enlargedPhoto");
+  const enlargeDescriptionModal = document.querySelector(".enlargedText");
+
+  enlargePhotoModal.src = enlargedPhoto;
+  enlargePhotoModal.alt = enlargedDescription;
+  enlargeDescriptionModal.textContent = enlargedDescription;
+
+  openModalEnlarge();
 };
 
 submitBtn.addEventListener("click", addUserCard);
