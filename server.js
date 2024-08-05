0c50e9efcfa272cd78e97b2d5e3a45d8a6d14377
Miroslav <mir.vbok@mail.ru>
2024-08-05 18:00:14 +0700

    все с начала

--- server.js
+++
@@ -1,9 +0,0 @@
-const next = require("next");
-const app = next({ dev: process.env.NODE_ENV !== "production" });
-
-app.prepare().then(() => {
-  const server = app.getRequestHandler();
-  server.listen(3000, () => {
-    console.log("Сервер запущен на порте 3000");
-  });
-});
--- test.tsx
+++
@@ -1,3 +0,0 @@
-import React from "react";
-
-const a = <div id="test">a</div>;
